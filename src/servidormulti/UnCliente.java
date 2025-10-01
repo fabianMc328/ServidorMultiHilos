571bb71154fbaeaa6617ef721d2e1140b42bc332
fabianMc328 <blackccks020@gmail.com>
2025-10-01 07:54:55 -0700

    primero commit

---
+++ src/servidormulti/UnCliente.java
@@ -0,0 +1,31 @@
+package servidormulti;
+import java.io.*;
+import java.net.Socket;
+import java.net.SocketException;
+  public class UnCliente implements Runnable {
+
+    final DataOutputStream salida;
+    final BufferedReader teclado = new BufferedReader(new InputStreamReader(System.in));
+    final DataInputStream entrada;
+
+    UnCliente(Socket s) throws IOException {
+        salida = new DataOutputStream(s.getOutputStream());
+        entrada = new DataInputStream(s.getInputStream());
+    }
+
+    @Override
+    public void run() {
+        String mensaje;
+        while (true) {
+            try {
+                mensaje = entrada.readUTF();
+                for (UnCliente cliente : ServidorMulti.clientes.values()) {
+                    cliente.salida.writeUTF(mensaje);
+                }
+            } catch (IOException ex) {
+
+            }
+        }
+    }
+}
+
