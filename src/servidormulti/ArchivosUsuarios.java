535f3287782ca44f7f05cc8f4cb387c4b58789ac
fabianMc328 <blackccks020@gmail.com>
2025-10-09 07:43:24 -0700

    codigo refactorizado en clases, metodo escribirUsuario y nueva clase de archivosUsuarios agregado

---
+++ src/servidormulti/ArchivosUsuarios.java
@@ -0,0 +1,60 @@
+package servidormulti;
+
+import java.io.*;
+
+public class ArchivosUsuarios {
+    private final File ArchivosUsuarios = new File("usuarios.txt");
+
+    public ArchivosUsuarios() {
+        try {
+            if (!ArchivosUsuarios.exists()) {
+                ArchivosUsuarios.createNewFile();
+
+            }
+        } catch (Exception e) {
+            System.out.println("Error al crear el archivo de usuarios");
+        }
+
+    }
+
+    public boolean escribirUsuarios(String usuario, String contra) {
+
+        try {
+            BufferedReader LecturaDeArchivo = new BufferedReader(new FileReader(ArchivosUsuarios));
+            String linea;
+            while ((linea = LecturaDeArchivo.readLine()) != null){
+                String[]partes = linea.split(":");
+                if (   (partes[0].length() > 0) &&  (partes[0].equals(usuario)) ){
+                    return false;
+
+                }
+            }
+
+        }catch (Exception e){
+            System.out.println("UPS, algo paso al leer el archivo");
+
+        }
+
+        try (PrintWriter escribir = new PrintWriter(new FileWriter(ArchivosUsuarios, true))){
+            escribir.println(usuario + ":" + contra);
+            return true;
+        }catch (Exception e){
+            System.out.println("Error al registrar a nuevo usuario");
+            return false;
+        }
+
+
+    }
+    public boolean VerificarLogin(String usuario, String contra) {
+
+        try {
+
+        }catch (Exception e){
+
+
+        }
+        return false;
+    }//llave del metodo
+
+
+}
