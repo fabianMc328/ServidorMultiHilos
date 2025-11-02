57a3403990f48060456f0863dc09845195363cab
fabian <blackccks020@gmail.com>
2025-11-02 16:07:11 -0700

    logica agregada para evitar que una vez iniciada la sesion de un usuario, no pueda iniciar sesion otro usuario con el mismo

--- src/servidormulti/ManejadorUsuarios.java
+++ src/servidormulti/ManejadorUsuarios.java
@@ -13,18 +13,38 @@ public class ManejadorUsuarios {
         String usuario = cliente.entrada.readUTF();
         String contra = cliente.entrada.readUTF();
         boolean exito = false;
+
         if (comando.equalsIgnoreCase("/register")) {
             exito = usuariosBD.registrarUsuario(usuario, contra);
             cliente.salida.writeUTF(exito ? "Usuario registrado correctamente." : "El usuario ya existe.");
-        } else if (comando.equalsIgnoreCase("/login")) {
-            exito = usuariosBD.verificarLogin(usuario, contra);
-            cliente.salida.writeUTF(exito ? "Sesión iniciada correctamente." : "El usuario no existe o contraseña incorrecta.");
-        }
-        if (exito) {
 
-            ServidorMulti.clientes.remove(cliente.getClienteId());
-            cliente.setNombreUsuario(usuario);
-            ServidorMulti.clientes.put(usuario, cliente);
+            if (exito) {
+                ServidorMulti.clientes.remove(cliente.getClienteId());
+                cliente.setNombreUsuario(usuario);
+                ServidorMulti.clientes.put(usuario, cliente);
+            }
+
+        } else if (comando.equalsIgnoreCase("/login")) {
+            boolean credencialesCorrectas = usuariosBD.verificarLogin(usuario, contra);
+
+            if (!credencialesCorrectas) {
+                cliente.salida.writeUTF("El usuario no existe o contraseña incorrecta.");
+                return false;
+            }
+            synchronized (ServidorMulti.clientes) {
+                if (ServidorMulti.clientes.containsKey(usuario)) {
+                    cliente.salida.writeUTF("Error: El usuario '" + usuario + "' ya está en línea.");
+                    return false;
+
+                } else {
+                    cliente.salida.writeUTF("Sesión iniciada correctamente.");
+                    ServidorMulti.clientes.remove(cliente.getClienteId());
+                    cliente.setNombreUsuario(usuario);
+                    ServidorMulti.clientes.put(usuario, cliente);
+
+                    exito = true;
+                }
+            }
         }
         return exito;
     }
